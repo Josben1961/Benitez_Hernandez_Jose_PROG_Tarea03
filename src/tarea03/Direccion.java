1bab1248e923edf47716d74eb825a728aa03a959
José S. Benítez Hernández <josben@movistar.es>
2017-11-03 11:58:39 +0100

    Añadimos el enumerador dirección

---
+++ src/tarea03/Direccion.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package tarea03;
+
+/**
+ * @author josben
+ *
+ */
+public enum Direccion {
+	//Estas son las direcciones del movimiento del rey
+	NORTE,NORESTE,ESTE,SURESTE,SUR,SUROESTE,OESTE,NOROESTE;
+
+}
