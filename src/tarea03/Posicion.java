02aca388213137c6747074d9a95a64d6ffe4adfd
José S. Benítez Hernández <josben@movistar.es>
2017-11-03 15:43:39 +0100

    Creamos los métodos getter y satter

--- src/tarea03/Posicion.java
+++ src/tarea03/Posicion.java
@@ -17,15 +17,46 @@ public class Posicion {
 			this.fila = fila;
 		} else {
 			this.fila = 1;
+			System.out.println("Error, la fila introducida no es correcta");
 		}
 		if (columna >= 'a' && columna <= 'h') {
 			this.columna = columna;
 		} else {
 			this.columna = 'a';
+			System.out.println("Error, la columna introducida no es correcta");
+		}
+		// Métodos getter
+	}
+
+	public int getFila() {
+		return fila;
+	}
+
+	public char getColumna() {
+		return columna;
+	}
+
+	// Métodos setter
+	public void setFila(int fila) {
+
+		if (fila > 0 && fila < 9) {
+			this.fila = fila;
+		} else {
+			this.fila = 1;
+		}
+		System.out.println("Error, la fila introducida no es correcta");
+	}
+
+	public void setColumna(char columna) {
+
+		if (columna > 'a' && columna < 'h') {
+			this.fila = fila;
+		} else {
+			this.fila = 1;
+
+			System.out.println("Error, la fila introducida no es correcta");
 		}
 
 	}
-
-	
 
 }
