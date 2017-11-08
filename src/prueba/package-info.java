a5c5f7e3b2cba850d26b573ab83c0001a488e838
José S. Benítez Hernández <josben@movistar.es>
2017-11-08 09:09:06 +0100

    Creamos el constructor Rey con el parámetro color

---
+++ src/prueba/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author josben
+ *
+ */
+package prueba;
--- src/tarea03/Rey.java
+++ src/tarea03/Rey.java
@@ -3,7 +3,10 @@ package tarea03;
 public class Rey {
 	private Color color;
 	private Posicion posicion;
-	
-	
+
+	public Rey(Color color) {
+		super();
+		this.color = color;
+	}
 
 }
