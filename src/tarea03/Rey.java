c91742270eb7b267242d76f4323fa80e29f7b9d9
José S. Benítez Hernández <josben@movistar.es>
2017-11-08 18:19:33 +0100

    Creo los métodos getter

--- src/tarea03/Rey.java
+++ src/tarea03/Rey.java
@@ -3,13 +3,35 @@ package tarea03;
 public class Rey {
 	private Color color;
 	private Posicion posicion;
+
+	// Constructor con el parámetro color
 	public Rey(Color color) {
-		
+
 		this.color = color;
 	}
-	
-	
 
+	// Constructor por defecto
+	public Rey() {
 
+		color = Color.BLANCO;
+
+	}
+
+	public Color getColor() {
+		return color;
+	}
+
+	public void setColor(Color color) {
+		this.color = color;
+	}
+
+	public Posicion getPosicion() {
+		return posicion;
+	}
+
+	public void setPosicion(Posicion posicion) {
+		this.posicion = posicion;
+	}
+	
 
 }
