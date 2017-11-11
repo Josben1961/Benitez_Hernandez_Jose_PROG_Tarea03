2eab4cf490b27288e4553975387a9ba492ca09b9
José S. Benítez Hernández <josben@movistar.es>
2017-11-11 08:44:41 +0100

    Corregimos el constructor con el parámetro color

--- src/tarea03/Rey.java
+++ src/tarea03/Rey.java
@@ -2,34 +2,53 @@ package tarea03;
 
 public class Rey {
 	private Color color;
+	private Posicion posicionFila;
+	private Posicion posicionColumna;
 	private Posicion posicion;
 
 	// Constructor con el parámetro color
 	public Rey(Color color) {
-
 		this.color = color;
+		switch (color) {
+		case BLANCO:
+			posicion = new Posicion(1, 'e');
+			break;
+		case NEGRO:
+			posicion = new Posicion(8, 'e');
+			break;
+		}
+
 	}
 
 	// Constructor por defecto
 	public Rey() {
 
 		color = Color.BLANCO;
+		posicionFila.setFila(1);
+		posicionColumna.setColumna('e');
 
 	}
-	// Métodos getters
 
 	public Color getColor() {
 		return color;
+	}
+
+	public void setColor(Color color) {
+		this.color = color;
 	}
 
 	public Posicion getPosicion() {
 		return posicion;
 	}
 
-	// Método toString
-
-	public String toString() {
-		return "Rey [color=" + color + ", posicion=" + posicion + "]";
+	public void setPosicion(Posicion posicion) {
+		this.posicion = posicion;
 	}
+
+	@Override
+	public String toString() {
+		return "Rey [getColor()=" + getColor() + ", getPosicion()=" + getPosicion() + "]";
+	}
+	
 
 }
